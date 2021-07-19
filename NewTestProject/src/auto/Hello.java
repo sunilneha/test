a81eefce61511523b251f85c312fef5d1c8b5d7c
sam <sunilsingh.jp@gmail.com>
2021-07-19 20:47:54 +0530

    Change the gickodriver

--- NewTestProject/src/auto/Hello.java
+++ NewTestProject/src/auto/Hello.java
@@ -1,9 +1,21 @@
 package auto;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium-JarFile\\chromedriver\\chromedriver.exe");
+		driver=new ChromeDriver();
+		//or
+		System.setProperty("webdriver.gecko.driver","D:\\Selenium-JarFile\\chromedriver\\geckodriver.exe");
+		 driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 
 	}
 
